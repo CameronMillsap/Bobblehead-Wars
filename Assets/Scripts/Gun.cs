644c2e6b065586af5b0b1d9d0d6235bf36cc3def
Cam Millsap <cam.millsap12@gmail.com>
2021-10-02 14:54:33 -0400

    BG Music, Gun, and Alien Sounds

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,12 +6,13 @@ public class Gun : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform launchPosition;
+    private AudioSource audioSource;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -39,5 +40,7 @@ public class Gun : MonoBehaviour
         //3
         bullet.GetComponent<Rigidbody>().velocity =
             transform.parent.forward * 100;
+
+        audioSource.PlayOneShot(SoundManager.Instance.gunFire);
     }
 }
