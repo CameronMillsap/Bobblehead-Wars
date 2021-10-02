861e18bebfefbbf27cd8bab2bd7e63a1d057e685
Cam Millsap <cam.millsap12@gmail.com>
2021-10-02 15:05:06 -0400

    Triple Gun Shot

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -7,6 +7,9 @@ public class Gun : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform launchPosition;
     private AudioSource audioSource;
+    public bool isUpgraded;
+    public float upgradeTime = 10.0f;
+    private float currentTime;
 
 
     // Start is called before the first frame update
@@ -33,14 +36,34 @@ public class Gun : MonoBehaviour
 
     void fireBullet()
     {
-        //1
-        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
-        //2
-        bullet.transform.position = launchPosition.position;
-        //3
-        bullet.GetComponent<Rigidbody>().velocity =
-            transform.parent.forward * 100;
+        Rigidbody bullet = createBullet();
+        bullet.velocity = transform.parent.forward * 100;
 
-        audioSource.PlayOneShot(SoundManager.Instance.gunFire);
+        if (isUpgraded)
+        {
+            Rigidbody bullet2 = createBullet();
+            bullet2.velocity = (transform.right + transform.forward / 0.5f) * 100;
+            Rigidbody bullet3 = createBullet();
+            bullet3.velocity = ((transform.right * -1) + transform.forward / 0.5f) * 100;
+
+            audioSource.PlayOneShot(SoundManager.Instance.upgradedGunFire);
+        }
+        else
+        {
+            audioSource.PlayOneShot(SoundManager.Instance.gunFire);
+        }
+    }
+
+    private Rigidbody createBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+        bullet.transform.position = launchPosition.position;
+        return bullet.GetComponent<Rigidbody>();
+    }
+
+    public void UpgradeGun()
+    {
+        isUpgraded = true;
+        currentTime = 0;
     }
 }
